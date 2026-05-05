00df092bfefdc74df864aab47eac05aaa1a7225b
MaiCoderHoon <buildgreat4wld@gmail.com>
2026-05-05 21:53:03 +0530

    Commit message: Updated main.py to include Path import and added patients.json file.

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
+
+def load_data():
+    with open("patients.json", "r") as f:
+        return json.load(f)
+    
 @app.get("/")
 
 def home():
-    return {"message": "Hello, World!"}
+    return {"message": "Patient Management System API"}
 
 @app.get("/about")
 def about():
-    return {"message":"This is done by Piyush" }
+    return {"message":"A fully functional API for managing patient records" }
+
+@app.get("/patients")
+def view_patients():
+    data = load_data()
+    return {"patients": data}
+
+@app.get("/patients/{patient_id}")
+def view_patients__id(patient_id: str = Path(..., description="The ID of the patient to retrieve",example = "P001")):
+    data = load_data()
+    if patient_id in data:
+        return {"patient" : data[patient_id]}
+    
+    return {"message": "Patient not found"}
